b6a0154e9bc475ca19b9a6c7f384560303c9f760
Vanilla <vanillaflamaster@gmail.com>
2024-03-31 00:46:04 +0300

    update index

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="LCU/websocket/event.d.ts" />
+/// <reference path="LCU/websocket/lol-gameflow/v1/gameflow-phase.d.ts" />
+/// <reference path="LCU/websocket/lol-matchmaking/v1/ready-check.d.ts" />
+
+/// <reference path="RCP/FE/CommonLibs.d.ts" />
